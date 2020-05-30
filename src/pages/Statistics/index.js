cde837fa1f4da89029485c613b3d2624288dbdbe
Rafael <rafael.inacio0196@gmail.com>
2020-05-30 20:31:50 -0300

    Screen statistics update front

--- src/pages/Statistics/index.js
+++ src/pages/Statistics/index.js
@@ -1,30 +1,45 @@
 import React, { useState, useEffect, useRef } from "react";
-import { MaterialIcons, Entypo, FontAwesome5 } from "@expo/vector-icons";
-import { StyleSheet, ScrollView, Text, FlatList, View, TouchableOpacity } from "react-native";
+import {
+  MaterialIcons,
+  Entypo,
+  FontAwesome5,
+  MaterialCommunityIcons,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
+import {
+  StyleSheet,
+  ScrollView,
+  Text,
+  FlatList,
+  View,
+  TouchableOpacity,
+  StatusBar
+} from "react-native";
 import { Badge } from "react-native-elements";
 import { Modalize } from "react-native-modalize";
 
 import {
   Wrapper,
   Header,
-  HeaderTitle,
-  HeaderTitleCountry,
-  CountryContainer,
-  NotificationsButton,
-  InfoContainer,
-  HeaderInfo,
-  InforTitle,
-  DetailsContainer,
-  CardContainer,
-  Card,
-  CardTitle,
-  CardDetails,
-  LongCardContainer,
-  LongCard,
-  LongCardTitle,
-  World,
-  WorldDetails,
-  TitleDetailsWorld,
+  TextHeader,
+  ActionNotification,
+  TextAtualization,
+  CountryName,
+  StatisticsContainer,
+  CardRecuperados,
+  TitleRecovered,
+  NumberCasesRecovered,
+  TitleAcompanhamento,
+  NumberCasesAcompanhamento,
+  CardConfirmados,
+  HeaderConfirmados,
+  TitleConfirmados,
+  InfoConfirmadosContainer,
+  DadosContainer,
+  NumberAcumulados,
+  TitleAcumulados,
+  NumberCasosNovos,
+  TitleCasosNovos,
 } from "./styles";
 
 import CurrentDate from "../../components/CurrentDate";
@@ -43,7 +58,7 @@ export default function Newsletter() {
       setCovidData(data);
     }
     getStatus();
-    console.log(covidData)
+    console.log(covidData);
     //Nao to conseguindo utilizar esses malditos dados;
   }, []);
 
@@ -53,139 +68,59 @@ export default function Newsletter() {
     modalizeRef.current?.open();
   }
 
+  StatusBar.setHidden(false)
   return (
     <Wrapper>
-      <ScrollView
-        showsHorizontalScrollIndicator={false}
-        showsVerticalScrollIndicator={false}
-      >
-        <Header>
-          <HeaderTitle>Situação Atual</HeaderTitle>
-          <NotificationsButton onPress={onOpen}>
-            <MaterialIcons
-              name="notifications"
-              size={30}
-              color="#eee"
-              style={{ marginRight: 5 }}
-            />
-            <Badge
-              status="error"
-              containerStyle={{
-                position: "absolute",
-                top: -4,
-                right: -4,
-                marginRight: 5,
-                opacity: notifications < 1 ? 0 : 9,
-              }}
-              value={notifications}
-            />
-          </NotificationsButton>
-        </Header>
-        <CountryContainer>
-          <HeaderTitleCountry>Brasil</HeaderTitleCountry>
-          <CurrentDate style={{ fontSize: 16, color: "#fff" }} />
-        </CountryContainer>
-        <InfoContainer>
-          <HeaderInfo>
-            <InforTitle>Últimas Atualizações</InforTitle>
-            <MaterialIcons
-              name="update"
-              size={24}
-              color="rgba(2, 157, 100, 0.7)"
-            />
-          </HeaderInfo>
-
-          <DetailsContainer>
-            <CardContainer>
-              <Card style={[styles.cardShadow, { backgroundColor: "#FEBF50" }]}>
-                <Entypo
-                  name="users"
-                  size={24}
-                  color="#fff"
-                  style={styles.iconCard}
-                />
-                <CardTitle>Contaminados</CardTitle>
-                <CardDetails>300</CardDetails>
-              </Card>
-              <Card style={[styles.cardShadow, { backgroundColor: "#F44336" }]}>
-                <MaterialIcons
-                  name="cancel"
-                  size={24}
-                  color="#fff"
-                  style={styles.iconCard}
-                />
-                <CardTitle>Fatalidades</CardTitle>
-                <CardDetails>230</CardDetails>
-              </Card>
-            </CardContainer>
-            <LongCardContainer>
-              <LongCard style={styles.cardShadow}>
-                <FontAwesome5 name="user-shield" size={24} color="#fff" />
-                <LongCardTitle>Recuperados</LongCardTitle>
-                <CardDetails>230</CardDetails>
-              </LongCard>
-            </LongCardContainer>
-            <World>
-              <WorldDetails>
-                <TitleDetailsWorld>Em todo o Brasil</TitleDetailsWorld>
-                <FlatList
-                  data={covidData}
-                  keyExtractor={(item) => item.uid}
-                  renderItem={({ item }) => {
-                    <TouchableOpacity key={item.uid}>
-                      <Text>estados</Text>
-                    </TouchableOpacity>;
-                  }}
-                />
-              </WorldDetails>
-            </World>
-          </DetailsContainer>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-          <Text>l</Text>
-        </InfoContainer>
-        <Modalize ref={modalizeRef}>
-          <Modal />
-        </Modalize>
-      </ScrollView>
+      <Header>
+        <TextHeader>Situação atual</TextHeader>
+        <ActionNotification onPress={onOpen}>
+          <MaterialCommunityIcons
+            name="notification-clear-all"
+            size={40}
+            color="#fff"
+          />
+          <Badge
+            status="error"
+            containerStyle={{
+              position: "absolute",
+              top: -4,
+              right: -4,
+              opacity: notifications === 0 ? 0 : 9,
+            }}
+            value={notifications}
+          />
+        </ActionNotification>
+      </Header>
+        <TextAtualization>Atualizado em: 23/23/23</TextAtualization>
+        <CountryName>Brasil</CountryName>
+        <CurrentDate style={{ paddingLeft: 15, color: "#fff" }} />
+        <StatisticsContainer>
+          <CardRecuperados style={styles.cardShadow}>
+            <TitleRecovered>Casos recuperados</TitleRecovered>
+            <NumberCasesRecovered>300.000</NumberCasesRecovered>
+            <TitleAcompanhamento>Em acompanhamento</TitleAcompanhamento>
+            <NumberCasesAcompanhamento>234.222</NumberCasesAcompanhamento>
+          </CardRecuperados>
+          <CardConfirmados>
+            <HeaderConfirmados>
+              <TitleConfirmados>CASOS CONFIRMADOS</TitleConfirmados>
+              <MaterialIcons name="arrow-downward" size={24} color="#5CBEA6" />
+            </HeaderConfirmados>
+            <InfoConfirmadosContainer>
+              <DadosContainer>
+                <NumberAcumulados>123.123</NumberAcumulados>
+                <TitleAcumulados>Acumulado</TitleAcumulados>
+              </DadosContainer>
+              <DadosContainer>
+                <NumberCasosNovos>12.111</NumberCasosNovos>
+                <TitleCasosNovos>Casos novos</TitleCasosNovos>
+              </DadosContainer>
+            </InfoConfirmadosContainer>
+          </CardConfirmados>
+        </StatisticsContainer>
+      <Modalize ref={modalizeRef}>
+        <Modal />
+      </Modalize>
     </Wrapper>
   );
 }
